7b8c5bdfa7dfc52faa8541bd2f9773a9631120e0
jamoliddinbe <bosuvay1@gmail.com>
2022-09-13 21:48:03 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,277 @@
+// birinci
+
+// var number = 3894782
+// if (number > 0 ){
+// console.log("Bu musbat son");
+// }else{
+//     console.log("Bu manfiy son");
+// }
+
+// ikkinci
+
+// var number = 17
+// if (number % 2 == 0  ){
+// console.log("Bu juft son");
+// } else{
+//     console.log("Bu toq son");
+// }
+
+// ucinci
+
+// var number = 10
+// if (number % 2 == 0  ){
+// console.log("Bu juft son");
+// } else{
+//     console.log("Bu toq son");
+// }
+
+// tortinci
+
+// var number1 = 4
+// var number2 = -6
+// if (number1 > 0 && number2 > 0){
+//     console.log("Bular musbat son");
+// } else {
+//     console.log("Orasida manfiy son ham bor");
+// }
+
+// bewinci
+
+// var number1 = 4
+// var number2 = 3
+// var number3 = 2
+// if (number1 > number2 && number2 > number3){
+//     console.log("1incisi hammasidan katta");
+// }else{
+//     console.log("masalada hatolik bor");
+// }
+
+// oltinci
+
+// var number1 = 3
+// var number2 = 6
+// var number3 = 9
+// if (number1 < number2 && number2 < number3){
+//     console.log("2inci ortada joylawgan");
+// }else{
+//     console.log(false);
+// }
+
+// yetinci
+
+// var number1 = 7
+// var number2 = 4
+// if (number1 % 2 == 0 ){
+//    console.log("Bu juft son");
+// } else {
+//     console.log("Bu toq son ");
+//  }
+// if (number2 % 2 == 0 ){
+//     console.log("Bu juft son");
+//  } else {
+//     console.log("Bu toq son ");
+//  }
+
+// sakkizinci
+
+// var number1 = 9
+// var number2 = 5
+// var number3 = 7
+// var number4 = 3
+// if (number1 > 0 && number2 > 0 && number3 > 0 && number4 > 0){
+//     console.log("Bu hamma sonlar musbat");
+// } else{
+//     console.log("Qaysdir son manfiy");
+// }
+
+// toqqizinci
+
+// var number1 = -5
+// var number2 = 8
+// var number3 = 2
+// if (number1 > 0 && number2 > 0 && number3 > 0){
+//     console.log("Hamma sonlar musbat");
+// } else {
+//     console.log("Orasida manfiy son bor");
+// }
+
+// oninci
+
+// var number1 = 1
+// var number2 = 2
+// var number3 = 3
+// var number4 = 4
+// var number5 = 5
+// var number6 = 6
+// var number7 = 7
+// if (number1 == 1 || number2 == 1 || number3 == 1 || number4 ==1 || n5 == 1 || number6 == 1 || number7 == 1){
+//     console.log("Monday");
+// } 
+// if( number2 == 2 || number3 == 2 || number4 ==2 || n5 == 2 || number6 == 2 || number7 == 2){
+//     console.log("Tuesday");
+// } 
+// if( number3 == 3 || number4 ==3 || n5 == 3 || number6 == 3 || number7 == 3){
+//     console.log("Wednesday");
+// }
+// if(number4 ==4 || n5 == 4 || number6 == 4 || number7 == 4){
+//     console.log("Thirsday");
+// } 
+// if(number5 == 5 || number6 == 5 || number7 == 5){
+//     console.log("Friday");
+// } 
+// if(number6 == 6 || number7 == 6){
+//     console.log("Saturday");
+// }
+// if(number7 == 7){
+//     console.log("Sunday");
+// }
+
+// onbirinci
+
+// var phoneNumber = 93
+// if (phoneNumber <= 93 && phoneNumber >= 90 ){
+//     console.log("Ucell");
+// } else if (phoneNumber <= 95 && phoneNumber >= 93 ){
+// console.log("Uzmobile");
+// } else if (phoneNumber <= 97 && phoneNumber >= 95 ){
+// console.log("UMS");
+// } else {
+//     console.log("BeeLine");
+// }
+
+// onikkinci
+
+// var number = 2
+// if (number > 0 ){
+//  console.log(number + 1);
+// } else {
+//     console.log(number-1);
+// }
+
+// onucinci
+
+// var number = 2
+// if (number > 0 ){
+//  console.log(number * 3 );
+// } else {
+//     console.log(number * 2 );
+// }
+
+// ontortinci
+
+// var number1 = 3
+// var number2 = 7
+// if (number1 > number2){
+//     console.log(" number1 kattaroq");
+// } else {
+//     console.log("number2 kattaroq");
+// }
+
+// onbewinci
+
+// var number1 = 1
+// var number2 = 2
+// var number3 = 3
+// if (number1 > number2 && number1 > number3 ){
+//     console.log("birincisi eng kattasi");
+// } else if (number2 > number1 && number2 >number3) {
+// console.log("ikkincisi eng kattasi");
+// } else{
+//     console.log("ucncisi eng kattasi");
+// }
+
+// onoltinci
+
+// var number1 = 1
+// var number2 = 2
+// var number3 = 3
+// if (number1 < number2 && number1 < number3 ){
+//     console.log("birincisi eng kicigi");
+// } else if (number2 < number1 && number2 < number3 ) {
+// console.log("ikkincisi eng kicigi");
+// } else{
+//     console.log("ucncisi eng kicigi");
+// }
+
+// onyetinci
+
+
+// onsakkizinci
+
+// var score = 5
+// if (score == 1){
+//     console.log("Sizga 100000 som ajratildi");
+// } else if (score == 2){
+// console.log("Sizga 200000 som ajratildi");
+// } else if (score == 3){
+//     console.log("Sizga 300000 som ajratildi");
+// } else if (score == 4){
+//     console.log("Sizga 400000 som ajratildi");
+// } else if (score == 5) {
+//     console.log("Sizga 500000 som ajratildi");
+// } else {
+//     console.log("Baho kiritishda hatolik yuz bergan, iltimos qaytadan urinib koring");
+// }
+
+// ontoqizzinci
+
+// var numberSample = 1
+// if (numberSample > 0 && numberSample % 2 == 0 ){
+//     console.log("Bu juft musbat son");
+// } else if (numberSample < 0 && numberSample % 2 == 0){
+//     console.log("Bu juft manfiy son");
+// } else if (numberSample > 0 && numberSample % 2 == 1){
+//     console.log("Bu toq musbat son");
+// } else {
+//     console.log("Bu toq manfiy son");
+// }
+
+// yigirmanci
+
+// var year = 2006
+// if (true){
+//     console.log(2022-year);
+// }
+
+// yigirmabirinci
+
+// var number1 = 1
+// var number2 = 2
+// var number3 = 3
+// if (number1 <= number2 && number1 <= number3 && number2 <= number3){
+//      console.log("Sonlar togri tartibda qoyilgan");
+// } else{
+//     console.log("Sonlar notogri tartibda qoyilgan");
+// }
+
+// yigirmaikkinci
+
+// var number1 = 1
+// var number2 = 2
+// if (number1 % 2 == 0 && number2 % 2 == 0 ){
+//     console.log("Ikkkala son ham  juft sondir");
+// } else {
+//     console.log("Qaysidir son toq son ekan");
+// }
+
+// yigirmaucinci
+
+// var number1 = 1
+// var number2 = 2
+// var number3 = 3
+// if (number1 > 0 && number2 > 0 && number3 > 0 ){
+//     console.log("Sonlarning hammasi musbat son");
+// } else{
+//     console.log("Qaysidur son manfiy");
+// }
+
+// yigirmatortinci
+
+// var number1 = 1
+// var number2 = 2
+// var number3 = 3
+// if (number1 > 0 || number2 > 0 || number3 > 0 ){
+//     console.log("Sonlarning qaysidur biri musbat son");
+// } else{
+//     console.log("Hamma son manfiy");
+// }
